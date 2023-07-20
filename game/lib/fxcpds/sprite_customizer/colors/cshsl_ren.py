13a3c7183b7de5b182a9db254ee11e69e529e1c8
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-07-20 15:27:04 -0400

    update docs

--- game/lib/fxcpds/sprite_customizer/colors/cshsl_ren.py
+++ game/lib/fxcpds/sprite_customizer/colors/cshsl_ren.py
@@ -1,6 +1,5 @@
 from .csrgb_ren import CSRGB
 from .cshsla_ren import CSHSLA
-from .csrgba_ren import CSRGBA
 
 
 """renpy
@@ -9,6 +8,12 @@ init python:
 
 
 class CSHSL:
+    """
+    HSL value container.
+
+    Provides methods for storing and dealing with HSL values.
+    """
+
     def __init__(self, hue: int, saturation: float, lightness: float):
         CSHSL._validate_percent('saturation', saturation)
         CSHSL._validate_percent('lightness', lightness)
@@ -30,14 +35,23 @@ class CSHSL:
 
     @property
     def hue(self) -> int:
+        """
+        The current hue value.
+        """
         return self._h
 
     @property
     def saturation(self) -> float:
+        """
+        The current saturation value.
+        """
         return self._s
 
     @property
     def lightness(self) -> float:
+        """
+        The current lightness value.
+        """
         return self._l
 
     ############################################################################
@@ -71,7 +85,7 @@ class CSHSL:
     def _to_rgb(self):
         if self._s == 0:
             tmp = int(self._l * 255)
-            return (tmp, tmp, tmp)
+            return tmp, tmp, tmp
         else:
             c = (1 - abs(2 * self._l - 1)) * self._s
             x = c * (1 - abs(((self._h / 60) % 2) - 1))
@@ -110,7 +124,7 @@ class CSHSL:
             g = int((g + m) * 255)
             b = int((b + m) * 255)
 
-        return (r, g, b)
+        return r, g, b
 
     ############################################################################
     #
@@ -119,21 +133,81 @@ class CSHSL:
     ############################################################################
 
     def set_hue(self, hue: int):
+        """
+        Sets the hue value.
+
+        The hue value will be corrected automatically, for example, if the value
+        `360` is given, the actual value set will be `0`; if the value `-12` is
+        set, the actual value set will be `348`
+
+        If the given value is not an int, this method will raise an exception.
+
+        Arguments
+        ---------
+        hue : int
+            The new hue value to set.
+        """
+        if not isinstance(hue, int):
+            raise Exception("'hue' must be an int value.")
         self._h = self._fix_hue(hue)
 
     def set_saturation(self, saturation: float):
+        """
+        Sets the saturation value.
+
+        If the saturation value not a float value between `0.0` and `1.0`
+        (inclusive) this method will raise an exception.
+        """
+        if not isinstance(saturation, float):
+            raise Exception("'saturation' must be a float value")
         CSHSL._validate_percent('saturation', saturation)
         self._s = saturation
 
     def set_lightness(self, lightness: float):
+        """
+        Sets the lightness value.
+
+        If the lightness value not a float value between `0.0` and `1.0`
+        (inclusive) this method will raise an exception.
+        """
+        if not isinstance(lightness, float):
+            raise Exception("'lightness' must be a float value")
         CSHSL._validate_percent('lightness', lightness)
         self._l = lightness
 
     def to_rgb(self) -> CSRGB:
+        """
+        Converts this HSL value to RGB and returns a new CSRGB instance.
+
+        Returns
+        -------
+        CSRGB
+            The converted CSRGB instance.
+        """
         tmp = self._to_rgb()
         return CSRGB(tmp[0], tmp[1], tmp[2])
 
     def to_hsla(self, alpha: float) -> CSHSLA:
+        """
+        Converts this HSL value to an HSLA value by adding an alpha channel set
+        to the given value.
+
+        If the alpha value not a float value between `0.0` and `1.0` (inclusive)
+        this method will raise an exception.
+
+        Arguments
+        ---------
+        alpha : float
+            The alpha value to set on the new CSHSLA instance.
+
+        Returns
+        -------
+        CSHSLA
+            A new CSHSLA instance with the same hue, saturation, and lightness
+            values as this CSHSL instance in addition to the given alpha value.
+            """
+        if not isinstance(alpha, float):
+            raise Exception("'alpha' must be a float value")
         CSHSL._validate_percent('a', alpha)
         return CSHSLA(self._h, self._s, self._l, alpha)
 
